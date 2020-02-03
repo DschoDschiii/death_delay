f867afe3d716ad1bdf6a917467613e309be0efde
%74T*27zIwC4o^@ <g.kofler@rolmail.net>
2020-02-03 15:31:00 +0100

    skeleton of the app

---
+++ lib/home.dart
@@ -0,0 +1,51 @@
+import 'package:death_delay/settings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() {
+    return new HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('DeathDelay'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.settings),
+                tooltip: 'Settings',
+                onPressed: () {
+//              nav to settings
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Settings()));
+                },
+              )
+            ],
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(text: ('First Aid')),
+                Tab(text: ('Second Aid'))
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Center(
+                child: Text('Uans'),
+              ),
+              Center(
+                child: Text('Zwoa'),
+              )
+            ],
+          )
+      ),
+    );
+  }
+}
---
+++ lib/settings.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+//        actions: <Widget>[
+//          IconButton(
+//            icon: Icon(Icons.exit_to_app),
+//            onPressed: () {
+//        //      Nav back to home
+//              Navigator.pop(context);
+//            },
+//          )
+//        ],
+      ),
+      body: Center(
+        child: Text('Some settings'),
+      ),
+    );
+  }
+}
